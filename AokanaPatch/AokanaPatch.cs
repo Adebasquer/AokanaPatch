1d53f47731912874a47ef431c16085ff683613be
Adebasquer <stuffanon@protonmail.com>
2021-04-17 02:32:05 +1200

    'init'

---
+++ AokanaPatch/AokanaPatch.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+using UnityEngine;
+
+namespace AokanaPatch
+{
+    [BepInPlugin("org.bepinex.plugins.aokanapatch", "Aokana Patch Plugin", "1.0.0.0")]
+    public class AokanaPatch : BaseUnityPlugin
+    {
+        // Awake is called once when both the game and the plug-in are loaded
+        void Awake()
+        {
+            UnityEngine.Debug.Log("Aokana patcher is active!");
+            Harmony harmony = new Harmony("org.bepinex.plugins.aokanapatch");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(AssetIO), "FileReadPGroup")]
+    class AssetIO_Patch
+    {
+        static byte[] getEncryptedFile(string path)
+        {
+            if (EngineMain.engine.pkmain == null)
+            {
+                return null;
+            }
+            return EngineMain.engine.pkmain.Get(path.ToLower());
+        }
+        static bool Prefix(ref byte[] __result, string path)
+        {
+
+            if (path[0] == '/')
+            {
+                path = path.Substring(1);
+            }
+            string fullPath = Path.Combine(Application.dataPath, path);
+            if (File.Exists(fullPath))
+            {
+                //UnityEngine.Debug.Log(fullPath);
+                FileStream fStream = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
+                byte[] blob = new byte[fStream.Length];
+                fStream.Read(blob, 0, blob.Length);
+                __result = blob;
+                return false;
+                
+            }
+            byte[] blob2 = getEncryptedFile(path);
+            if (blob2 == null) { return false; }
+
+            if (blob2.Length == 0)
+            {
+                //If file is not found in *.dat files or in unencrypted files, don't let original method run
+                return false;
+            } else
+            {
+                //If file is found in *.dat files but not in unencrypted files, let the original method run (no replacement was found in unencrypted files)
+                return true;
+            }
+            
+        }
+    }
+}
---
+++ AokanaPatch/README.md
@@ -0,0 +1,19 @@
+# AokanaNoDatPatch
+An Aokana patcher made using BepInEx which allows the game to read assets directly from .../Aokana/Aokana_Data instead of from the encrypted .../Aokana/Aokana_Data/\*.dat files. 
+
+If no unencrypted file with the correct folder structure is found in .../Aokana/Aokana_Data, the patch gets data from the game's encrypted \*.dat files the using the game's methods.
+
+# Installation:
+- Install BepInEx
+https://bepinex.github.io/bepinex_docs/v5.0/articles/user_guide/installation.html
+
+- Place AokanaPatch.dll in .../Aokana/BepInEx/Plugins/
+
+# Usage:
+As an example, edit .../Aokana/Aokana_Data/ui_hi/en/sgtitle990000.webp however you like and test that it displays on the title screen. 
+
+The game has some checks for images of the wrong dimension and such so you might have to edit the original image if you get a black title screen or crash. If you don't have this folder or the original image, run my older data decryption tool and copy the results into .../Aokana/Aokana_Data
+
+![image](https://user-images.githubusercontent.com/74600302/115036070-48db4980-9f21-11eb-9362-49ff5c15c8df.png)
+
+This patch is intended for translators and others to change the game's contents easily without re-encrypting any files.
